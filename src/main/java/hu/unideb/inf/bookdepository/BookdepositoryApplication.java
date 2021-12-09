3cc0e2c9d96e27224c2f31c148ce5be4d8f129d2
Péter Hegedűs <hpeti0912@gmail.com>
2021-12-09 14:22:36 +0100

    Initial commit, generated from spring initalizer.

---
+++ src/main/java/hu/unideb/inf/bookdepository/BookdepositoryApplication.java
@@ -0,0 +1,13 @@
+package hu.unideb.inf.bookdepository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookdepositoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookdepositoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/unideb/inf/bookdepository/BookdepositoryApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.unideb.inf.bookdepository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookdepositoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
